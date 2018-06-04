acd5b24286b2d42162a0e340d8c868c42cc0018e
Michael Beaton <beatonma@gmail.com>
2018-06-04 15:45:39 +0100

    Removed typing by databinding

--- src/main/java/org/beatonma/lib/ui/activity/BaseActivity.java
+++ src/main/java/org/beatonma/lib/ui/activity/BaseActivity.java
@@ -4,15 +4,14 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import org.beatonma.lib.log.Log;
 
-import java.lang.ref.WeakReference;
-
 import androidx.annotation.CallSuper;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.util.Pair;
@@ -25,19 +24,15 @@ import androidx.databinding.ViewDataBinding;
  * convenience methods for databinding, snackbars and animations.
  * Also creates a weak context.
  */
-public abstract class BaseActivity<T extends ViewDataBinding> extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
     protected final static String TAG = "BaseActivity";
 
     public final static String THEME_PREFS = "app";
     public final static String UI_DARK_THEME = "pref_dark_theme";
 
-    protected WeakReference<BaseActivity<T>> mWeakContext;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        mWeakContext = new WeakReference<>(this);
 
         final SharedPreferences prefs = getSharedPreferences(THEME_PREFS, MODE_PRIVATE);
         final boolean forceNight = prefs.getBoolean(UI_DARK_THEME, false);
@@ -50,30 +45,24 @@ public abstract class BaseActivity<T extends ViewDataBinding> extends AppCompatA
         initIntent(getIntent());
     }
 
-    protected T setLayout(int layoutId) {
+    protected ViewDataBinding setLayout(int layoutId) {
         return DataBindingUtil.setContentView(this, layoutId);
     }
 
-    protected abstract T getBinding();
-
-    public com.google.android.material.snackbar.Snackbar getSnackbar(final String message) {
+    public Snackbar getSnackbar(final String message) {
         final View v = findViewById(R.id.top_level_container);
         if (v != null) {
-            return com.google.android.material.snackbar.Snackbar.make(v, message, com.google.android.material.snackbar.Snackbar.LENGTH_LONG);
+            return Snackbar.make(v, message, Snackbar.LENGTH_LONG);
         }
         Log.e(TAG, "Error building snackbar for message: " + message);
         return null;
     }
 
     public void showSnackbar(final String message) {
-        final com.google.android.material.snackbar.Snackbar snackbar = getSnackbar(message);
+        final Snackbar snackbar = getSnackbar(message);
         if (snackbar != null) {
             snackbar.show();
         }
-    }
-
-    public WeakReference<BaseActivity<T>> getWeakContext() {
-        return mWeakContext;
     }
 
     /**
@@ -83,16 +72,17 @@ public abstract class BaseActivity<T extends ViewDataBinding> extends AppCompatA
      *           but Activities should include this stub in their layout
      *           for better performance
      */
+    @Nullable
     public View getSharedViewStub() {
-        View v = findViewById(R.id.stub);
-        if (v == null) {
-            View parent = getBinding().getRoot();
-            if (parent instanceof ViewGroup) {
-                v = LayoutInflater.from(this).inflate(R.layout.stub, (ViewGroup) parent);
-                Log.w(TAG, "Injected a stub view into layout parent for activity transition - please include a stub in the activity layout file to improve performance!");
-            }
-        }
-        return v;
+        return findViewById(R.id.stub);
+//        if (v == null) {
+//            View parent = getBinding().getRoot();
+//            if (parent instanceof ViewGroup) {
+//                v = LayoutInflater.from(this).inflate(R.layout.stub, (ViewGroup) parent);
+//                Log.w(TAG, "Injected a stub view into layout parent for activity transition - please include a stub in the activity layout file to improve performance!");
+//            }
+//        }
+//        return v;
     }
 
     public Pair<View, String>[] getSharedViews() {
