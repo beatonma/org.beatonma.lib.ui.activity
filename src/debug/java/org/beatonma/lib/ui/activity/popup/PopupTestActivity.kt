ebbb68fb0394ea4fef8cfb6934bdda0f0e6ce08c
Michael Beaton <beatonma@gmail.com>
2018-07-27 18:41:08 +0100

    Implemented methods to allow tests to change popup layout

--- src/debug/java/org/beatonma/lib/ui/activity/popup/PopupTestActivity.kt
+++ src/debug/java/org/beatonma/lib/ui/activity/popup/PopupTestActivity.kt
@@ -3,64 +3,130 @@
 package org.beatonma.lib.ui.activity.popup
 
 import android.view.View
+import androidx.annotation.UiThread
 import androidx.databinding.ViewDataBinding
 import org.beatonma.lib.ui.activity.R
 import org.beatonma.lib.ui.activity.startActivity
-import org.beatonma.lib.util.kotlin.extensions.pxToDp
-import org.beatonma.lib.util.kotlin.extensions.toast
 
-class SmallPopupTestActivity : PopupActivity() {
+
+abstract class BaseTestPopup: PopupActivity() {
+    override fun initContentLayout(binding: ViewDataBinding) {
+        showButtons()
+        showTitle()
+    }
+
+    fun showButtons(show: Boolean = true) {
+        if (show) {
+            showPositiveButton()
+            showNegativeButton()
+            showCustomActionButton()
+        } else hideButtons()
+    }
+    abstract fun showTitle(show: Boolean = true)
+
+    @UiThread abstract fun showPositiveButton()
+    @UiThread abstract fun showNegativeButton()
+    @UiThread abstract fun showCustomActionButton()
+
+    fun hideButtons() {
+        runOnUiThread {
+            onPositiveClick()
+            onNegativeClick()
+            onCustomActionClick()
+        }
+    }
+}
+
+class SmallPopupTestActivity : BaseTestPopup() {
     override val contentLayoutID: Int
         get() = R.layout.popup_small
 
-    override fun initContentLayout(binding: ViewDataBinding) {
-        setTitle("Small text")
-        onPositiveClick("+", listener = View.OnClickListener { view ->
-            view.startActivity(MediumPopupTestActivity::class)
-        })
-        onNegativeClick("-", listener = View.OnClickListener { onBackPressed() })
-        onCustomActionClick("#", listener = View.OnClickListener {  })
 
-        binding.root.post {
-            toast("scrollview height: ${pxToDp(findViewById<View>(R.id.content_scroll_wrapper).measuredHeight)}dp")
+    override fun showPositiveButton() {
+        runOnUiThread {
+            onPositiveClick("+", listener = View.OnClickListener { view ->
+                view.startActivity(MediumPopupTestActivity::class)
+            }, animate = false)
+        }
+    }
+
+    override fun showNegativeButton() {
+        runOnUiThread {
+            onNegativeClick("-", listener = View.OnClickListener { onBackPressed() }, animate = false)
+        }
+    }
+
+    override fun showCustomActionButton() {
+        runOnUiThread {
+            onCustomActionClick("#", listener = View.OnClickListener { }, animate = false)
+        }
+    }
+
+    override fun showTitle(show: Boolean) {
+        runOnUiThread {
+            setTitle(if (show) "Small text" else null)
         }
     }
 }
 
-class MediumPopupTestActivity : PopupActivity() {
+class MediumPopupTestActivity : BaseTestPopup() {
     override val contentLayoutID: Int
         get() = R.layout.popup_medium
 
-    override fun initContentLayout(binding: ViewDataBinding) {
-        setTitle("Medium text")
-        onPositiveClick("+", listener = View.OnClickListener {view ->
-            view.startActivity(LargePopupTestActivity::class)
-        })
-        onNegativeClick("-", listener = View.OnClickListener { onBackPressed() })
-        onCustomActionClick("#", listener = View.OnClickListener {  })
-
-        binding.root.post {
-            toast("scrollview height: ${pxToDp(findViewById<View>(R.id.content_scroll_wrapper).measuredHeight)}dp")
+    override fun showPositiveButton() {
+        runOnUiThread {
+            onPositiveClick("+", listener = View.OnClickListener { view ->
+                view.startActivity(LargePopupTestActivity::class)
+            }, animate = false)
         }
     }
 
+    override fun showNegativeButton() {
+        runOnUiThread {
+            onNegativeClick("-", listener = View.OnClickListener { onBackPressed() }, animate = false)
+        }
+    }
 
+    override fun showCustomActionButton() {
+        runOnUiThread {
+            onCustomActionClick("#", listener = View.OnClickListener { }, animate = false)
+        }
+    }
+
+    override fun showTitle(show: Boolean) {
+        runOnUiThread {
+            setTitle(if (show) "Medium text" else null)
+        }
+    }
 }
 
-class LargePopupTestActivity : PopupActivity() {
+class LargePopupTestActivity : BaseTestPopup() {
     override val contentLayoutID: Int
         get() = R.layout.popup_large
 
-    override fun initContentLayout(binding: ViewDataBinding) {
-        setTitle("Long text")
-        onPositiveClick("+", listener = View.OnClickListener { view ->
-            view.startActivity(SmallPopupTestActivity::class)
-        })
-        onNegativeClick("-", listener = View.OnClickListener { onBackPressed() })
-        onCustomActionClick("#", listener = View.OnClickListener {  })
+    override fun showPositiveButton() {
+        runOnUiThread {
+            onPositiveClick("+", listener = View.OnClickListener { view ->
+                view.startActivity(SmallPopupTestActivity::class)
+            }, animate = false)
+        }
+    }
 
-        binding.root.post {
-            toast("scrollview height: ${pxToDp(findViewById<View>(R.id.content_scroll_wrapper).measuredHeight)}dp")
+    override fun showNegativeButton() {
+        runOnUiThread {
+            onNegativeClick("-", listener = View.OnClickListener { onBackPressed() }, animate = false)
+        }
+    }
+
+    override fun showCustomActionButton() {
+        runOnUiThread {
+            onCustomActionClick("#", listener = View.OnClickListener { }, animate = false)
+        }
+    }
+
+    override fun showTitle(show: Boolean) {
+        runOnUiThread {
+            setTitle(if (show) "Large text" else null)
         }
     }
 }
